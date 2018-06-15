5ae5121e5ccb9dcdf2a301d49241d5f3a05a37f4
timdupa <36054863+timdupa@users.noreply.github.com>
2018-06-15 19:31:55 -0400

    Established index.html on localhost

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var router = express.Router(); 
+
+var publicPath = path.resolve(__dirname, "static");
+app.use(express.static(publicPath));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(8000, function(){
+	console.log('listening at Port 8000');
+});
+
+app.get('/', function(req, res){
+	res.sendFile(`${publicPath}/index.html`);
+});
